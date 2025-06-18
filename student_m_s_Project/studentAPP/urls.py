e7b3823ebaee599c03781fb06ecc4a77715ee2c0
Iman-Datta <dattaiman56@gmail.com>
2025-06-19 01:39:51 +0530

    StudentAPP connected

---
+++ student_m_s_Project/studentAPP/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from studentAPP import views
+
+urlpatterns = [
+    path('home/', views.home, name='_home'),
+    path('result/', views.result, name='_result'),
+    path('payment/', views.payment, name='_payment'),
+    path('registration/', views.registration, name='_registration'),
+]
--- student_m_s_Project/studentAPP/views.py
+++ student_m_s_Project/studentAPP/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
 
-# Create your views here.
+def home(request: HttpResponse):
+    context = {
+        'title': 'Home Page',
+        'message': 'Welcome to the Home Page!'
+    }
+    return render(request, 'index.html', context)
+
+def result(request: HttpResponse):
+    context = {
+        'title': 'Result Page',
+        'message': 'This is the Result Page!'
+    }
+    return render(request, 'result.html', context)
+
+def payment(request: HttpResponse):
+    context = {
+        'title': 'Payment Page',
+        'message': 'This is the Payment Page!'
+    }
+    return render(request, 'payment.html', context)
+
+def registration(request: HttpResponse):
+    context = {
+        'title': 'Registration Page',
+        'message': 'This is the Registration Page!'
+    }
+    return render(request, 'registration.html', context)
