5a03b0b6b151e930748cd3deaa59f471221752d0
Iman-Datta <dattaiman56@gmail.com>
2025-06-20 13:15:47 +0530

    create account page

--- student_m_s_Project/authAPP/urls.py
+++ student_m_s_Project/authAPP/urls.py
@@ -3,6 +3,6 @@ from authAPP import views
 
 urlpatterns = [
     path('login/', views.login, name='_login'),
-    path('creare_account/', views.create_account, name='_create_account'),
+    path('create_account/', views.create_account, name='_create_account'),
     path('logout/', views.logout, name='_logout'),
 ]
--- student_m_s_Project/authAPP/views.py
+++ student_m_s_Project/authAPP/views.py
@@ -8,11 +8,42 @@ def login(request: HttpResponse):
     }
     return render(request, 'login.html', context)
 def create_account(request: HttpResponse):
-    context = {
-        'title': 'Create Account Page',
-        'message': 'This is the Create Account Page!'
-    }
-    return render(request, 'create_account.html', context)
+    if request.method == 'POST':
+        context = {
+            'title': 'Create Account',
+            'message': 'Please fill in the form below to create your account.'
+        }
+        first_name = request.POST.get('first_name')
+        middle_name = request.POST.get('middle_name')
+        last_name = request.POST.get('last_name')
+        email = request.POST.get('email')
+        mobile = request.POST.get('mobile')
+        father_name = request.POST.get('father_name')
+        mother_name = request.POST.get('mother_name')
+        guardian_contact = request.POST.get('guardian_contact')
+        permanent_address = request.POST.get('permanent_address')
+        permanent_city  = request.POST.get('permanent_city')
+        permanent_state = request.POST.get('permanent_state')
+        permanent_zip = request.POST.get('permanent_zip')
+        current_address = request.POST.get('current_address')
+        current_city = request.POST.get('current_city')
+        current_state = request.POST.get('current_state')
+        current_zip = request.POST.get('current_zip')
+
+# Just checking if the form is submitted
+        print(f"First Name: {first_name}")
+        print(f"Middle Name: {middle_name}")
+        print(f"Last Name: {last_name}")
+        print(f"Email: {email}")
+        print(f"Mobile: {mobile}")
+        print(f"Father's Name: {father_name}")
+        print(f"Mother's Name: {mother_name}")
+        print(f"Guardian Contact: {guardian_contact}")
+        print(f"Permanent Address: {permanent_address}, {permanent_city}, {permanent_state}, {permanent_zip}")
+        print(f"Current Address: {current_address}, {current_city}, {current_state}, {current_zip}")
+
+        return render(request, 'create_account.html', context)
+    return render(request, 'create_account.html')
 def logout(request: HttpResponse):
     context = {
         'title': 'Logout Page',
