9a5eef8fb2af075ecede1e2c23ac7919dc6c8ba3
Iman-Datta <dattaiman56@gmail.com>
2025-06-28 00:52:52 +0530

    TeacherAPP

--- student_m_s_Project/studentAPP/views.py
+++ student_m_s_Project/studentAPP/views.py
@@ -1,11 +1,28 @@
+
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .models import Student, Marksheet
 
+@login_required
 def result(request: HttpResponse):
-    context = {
-        'title': 'Result Page',
-        'message': 'This is the Result Page!'
-    }
+    student = request.user.student
+    if not student:
+        messages.error(request, 'You are not registered as a student.')
+        return redirect('_login')
+    else:
+        fnm = request.user.first_name
+        lnm = request.user.last_name
+        reg_number = request.user.username  # this is your REGxxxx
+        marksheets = Marksheet.objects.filter(student=student)
+
+        context = {
+            'title': 'Result Page',
+            'name': f'{fnm} {lnm}',
+            'reg_number': reg_number,
+            'marksheets': marksheets
+        }
     return render(request, 'studentAPP/result.html', context)
 
 def payment(request: HttpResponse):
